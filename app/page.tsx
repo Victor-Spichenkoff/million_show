32470940d60b72fae529590eef7c70a8f80f2dca
Victor-Spichenkoff <victorspich13@gmail.com>
2025-04-08 18:38:28 -0300

    Choose theme

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ import Image from "next/image";
 export default function Home() {
   
   return (
-    <div className="flex flex-col items-center text-2xl bg-back" >
-      <h1 className="text-primary">PRimary</h1>
+    <div className="flex flex-col items-center text-2xl bg-back h-screen w-screen" >
+      <h1 className="text-text">PRimary</h1>
       <h1 className="text-secondary">Secondary</h1>
-      <h1 className="text-highlight">highlight</h1>
+      <h1 className="text-hightlight">highlight</h1>
       <div className="bg-success w-12 h-12"></div>
       <div className="bg-error w-12 h-12"></div>
       <ThemeToggle />
