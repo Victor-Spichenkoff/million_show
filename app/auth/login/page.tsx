6869d23960ba5fd887287cf170b407a383bf6238
Victor-Spichenkoff <victorspich13@gmail.com>
2025-04-14 21:32:50 -0300

    Fix suspense import

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -5,6 +5,7 @@ import Image from "next/image"
 import GoldImage from "@/assets/images/gold.jpg"
 import {Header} from "@/components/template/header";
 import {Footer} from "@/components/template/footer";
+import {Suspense} from "react";
 
 export default function LoginScreen() {
     return (
