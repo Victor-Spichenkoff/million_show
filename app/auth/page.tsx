4421a8e01a628f68327c3ff15ebabfcedf0abbc1
Victor-Spichenkoff <victorspich13@gmail.com>
2025-07-11 20:44:17 -0300

    Fix useSearch by creating extra component

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -7,35 +7,12 @@ import {Suspense, useEffect, useState} from "react";
 import Link from "next/link";
 import {CreateForm} from "@/components/auth/createForm";
 import {useSearchParams} from "next/navigation";
-import {uesIsCreationPage} from "@/hooks/useIsCreationPage";
+import {AuthPage} from "@/app/auth/authPage";
 
-export default function LoginScreen() {
-    const {isLogin, setIsLogin } = uesIsCreationPage()
-
+export default function AuthScreen() {
     return (
-            <div className={"flex flex-col justify-between items-center h-full"}>
-                <Header label={"Login"}/>
-                <div
-                    className={"flex flex-col-reverse md:flex-row max-w-[400px] min-h-[600px] md:max-w-[1000px] md:min-h-0 " +
-                        " bg-primary mx-3 rounded-4xl overflow-hidden shadow-lg shadow-black/30"}
-                >
-                    <Suspense>
-                        {isLogin ? (
-                            <LoginForm setIsLogin={setIsLogin}/>
-
-                        ) : <CreateForm setIsLogin={setIsLogin}/>}
-                    </Suspense>
-                    <div
-                        className={"flex-1 bg-gold min-h-[205] amd:amin-w-[550px] lg:min-w-[550px] shadow-black/40 shadow-md"}>
-                        <div style={{
-                            backgroundImage: 'url("/gold.jpg")',
-                            backgroundSize: "cover",
-                            backgroundPosition: "center"
-                        }} className={"w-full h-full"}>
-                        </div>
-                    </div>
-                </div>
-                <Footer notAbsolute/>
-            </div>
+        <Suspense>
+            <AuthPage/>
+        </Suspense>
     )
 }
